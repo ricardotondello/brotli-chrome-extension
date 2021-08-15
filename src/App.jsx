eea2776d1cc7b14d6dbd9605d4831fcf7aeb1106
Ricardo Tondello <ricardo.tondello@betssongroup.com>
2021-08-15 12:01:54 +0200

    decompress from base64 strings

--- src/App.jsx
+++ src/App.jsx
@@ -17,21 +17,53 @@ const App = () => {
         result += Buffer.from(str[i++]).toString('hex')
       }
     }
-
+    
     return Buffer.from(result, 'hex');
   }
 
+  function base64ToHex(str) {
+    const raw = atob(str);
+    let result = '';
+    for (let i = 0; i < raw.length; i++) {
+      const hex = raw.charCodeAt(i).toString(16);
+      result += (hex.length === 2 ? hex : '0' + hex);
+    }
+    var resultString = result.toUpperCase();
+    return Buffer.from(resultString, "hex");
+  }
+
+  function ArrayToJson(decompressed){
+    var res = '';
+    var chunk = 8 * 1024;
+    var i;
+    for (i = 0; i < decompressed.length / chunk; i++) {
+      res += String.fromCharCode.apply(null, decompressed.slice(i * chunk, (i + 1) * chunk));
+    }
+    res += String.fromCharCode.apply(null, decompressed.slice(i * chunk));
+    return res;
+  }
+
   function decompressFunction() {
+    var str = document.getElementById("hash").value;
+
     try {
-      var str = document.getElementById("hash").value;
-      var b = xToBuffer(str);
-      var decompressed = decompress(b);
-      
-      var json = String.fromCharCode.apply(String, decompressed);
-     
-      return json;
+      //try array of string first
+      var bufferArray = xToBuffer(str);
+      var decompressed = decompress(bufferArray);
+
+      return ArrayToJson(decompressed);
     } catch (e) {
-      return e.message;
+    }
+
+    try {
+      //try with base64
+      var bufferArray = base64ToHex(str);
+      
+      var decompressed = decompress(bufferArray);
+
+      return ArrayToJson(decompressed);
+    } catch (error) { 
+      return 'Could not decompress: ' + error.message;
     }
   }
 
@@ -39,7 +71,7 @@ const App = () => {
     <div>
       <h4>Brotli decompress</h4>
       <br />
-      <textarea id="hash" name="hash" rows="10" cols="100" placeholder="Paste your array of bytes here..."></textarea>
+      <textarea id="hash" name="hash" rows="10" cols="100" placeholder="Paste your array of bytes here or base64..."></textarea>
       <textarea id="hashdecompressed" name="hashdecompressed" rows="10" cols="100" placeholder="Result" value={valueDecompressed}></textarea>
       <button id="decompress_btn" onClick={() => { setValueDecompressed(decompressFunction) }}>Decompress</button>
       <p><i>Powered by Ricardo Tondello (@rito01)</i></p>
